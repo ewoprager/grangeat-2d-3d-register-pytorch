e3f96c29ac441a69c9bb1851e58f2f8e4ecdbb75
ewoprager <ep585@cam.ac.uk>
2025-02-03 15:27:59 +0000

    cache directory creation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import torch
 import matplotlib.pyplot as plt
@@ -10,8 +11,11 @@ import test_radon3d
 from register import register
 
 if __name__ == "__main__":
+    cache_directory = "cache"
+    if not os.path.exists(cache_directory):
+        os.makedirs(cache_directory)
     # test_radon2d.benchmark_radon2d("/home/eprager/Documents/Data/4th year project/First/x_ray/x_ray.dcm")
     # test_radon2d.benchmark_dRadon2dDR("/home/eprager/Documents/Data/4th year project/First/x_ray/x_ray.dcm")
     # test_radon3d.benchmark_radon3d(sys.argv[1])
     # test_radon3d.benchmark_dRadon3dDR(sys.argv[1])
-    register(sys.argv[1], cache_directory="cache", load_cached=True, regenerate_drr=True)
+    register(sys.argv[1], cache_directory=cache_directory, load_cached=True, regenerate_drr=True)
