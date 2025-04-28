5e96b3b8b6165978d6b90a98bd59465ca1e1757d
Edmund Prager <pragere12@gmail.com>
2025-04-28 17:57:25 +0100

    started on some more docstrings

--- Extension/include/Similarity.h
+++ Extension/include/Similarity.h
@@ -4,8 +4,16 @@
 
 namespace ExtensionTest {
 
+/**
+ * @param a A tensor of `torch.float32`s of any size
+ * @param b A tensor of `torch.float32`s matching `a` in size
+ * @return The zero-normalised cross-correlation between the given tensors.
+ */
 at::Tensor NormalisedCrossCorrelation_CPU(const at::Tensor &a, const at::Tensor &b);
 
+/**
+ * An implementation of `NormalisedCrossCorrelation_CPU` that uses CUDA parallisation.
+ */
 __host__ at::Tensor NormalisedCrossCorrelation_CUDA(const at::Tensor &a, const at::Tensor &b);
 
 struct Similarity {
@@ -18,7 +26,6 @@ struct Similarity {
 		TORCH_INTERNAL_ASSERT(a.device().type() == device)
 		TORCH_INTERNAL_ASSERT(b.device().type() == device)
 	}
-
 };
 
 } // namespace ExtensionTest
--- Extension/include/Texture.h
+++ Extension/include/Texture.h
@@ -5,9 +5,14 @@
 
 namespace ExtensionTest {
 
+/**
+ * An enumeration of address modes for texture objects, similar to the argument `padding_mode` in
+ * `torch.nn.functional.grid_sample`, or the CUDA enumeration `cudaTextureAddressMode`.
+ */
 enum class TextureAddressMode {
-	ZERO,
-	WRAP
+	ZERO, ///< Sampling locations outside texture coordinate range will be read as 0
+	WRAP ///< Sampling locations outside texture coordinate range will be read wrapped back according to ((x - left) mod
+		 ///< width + left, (y - bottom) mod height + bottom, etc...
 };
 
 #ifdef __CUDACC__
@@ -23,35 +28,58 @@ inline cudaTextureAddressMode TextureAddressModeToCuda(TextureAddressMode tam) {
 }
 #endif
 
-template <std::size_t dimensionality, typename intType=int, typename floatType=float> class Texture {
-public:
+/**
+ * @brief A parent texture class containing template data and functionality.
+ * @tparam dimensionality The integer dimensionality of the texture
+ * @tparam intType The type used to express integer values like the dimensions of the texture
+ * @tparam floatType The type used to express floating-point values like the spacing of the texture's values in world
+ * coordinates
+ *
+ * This class is not an interface, rather it is templated to store some values and implement some functionality common
+ * to all texture objects. It is intended to be used as a fully template-specified base class for any texture object.
+ */
+template <std::size_t dimensionality, typename intType = int, typename floatType = float> class Texture {
+  public:
 	using SizeType = Vec<intType, dimensionality>;
 	using VectorType = Vec<floatType, dimensionality>;
 	using AddressModeType = Vec<TextureAddressMode, dimensionality>;
 
+	/// Get the dimensions of the texture
 	[[nodiscard]] __host__ __device__ const SizeType &Size() const { return size; }
+	/// Get the spacing of the texture's values in world coordinates
 	[[nodiscard]] __host__ __device__ const VectorType &Spacing() const { return spacing; }
+	/// Get the position of the centre of the texture in world coordinates
 	[[nodiscard]] __host__ __device__ const VectorType &CentrePosition() const { return centrePosition; }
 
+	/**
+	 * @return The size of the texture in world coordinates
+	 */
 	[[nodiscard]] __host__ __device__ VectorType SizeWorld() const {
 		return size.template StaticCast<floatType>() * spacing;
 	}
 
+	/**
+	 * @param index A location in the texture data
+	 * @return Whether the location is within the bounds of the texture's data's size
+	 */
 	[[nodiscard]] __host__ __device__ bool In(const SizeType &index) const {
 		return (index >= SizeType{}).BooleanAll() && (index < size).BooleanAll();
 	}
 
+	/**
+	 * @return The `Linear` mapping from world coordinates to texture coordinates, according to the texture's world
+	 * position and value spacing.
+	 */
 	[[nodiscard]] __host__ __device__ Linear<VectorType> MappingWorldToTexCoord() const {
 		const VectorType sizeWorldInverse = floatType{1.} / SizeWorld();
 		return {VectorType::Full(floatType{.5}) - centrePosition * sizeWorldInverse, sizeWorldInverse};
 	}
 
-protected:
+  protected:
 	Texture() = default;
 
-	Texture(SizeType _size, VectorType _spacing, VectorType _centrePosition = {}) : size(_size), spacing(_spacing),
-		centrePosition(_centrePosition) {
-	}
+	Texture(SizeType _size, VectorType _spacing, VectorType _centrePosition = {})
+		: size(_size), spacing(_spacing), centrePosition(_centrePosition) {}
 
 	// yes copy
 	Texture(const Texture &) = default;
@@ -63,10 +91,10 @@ protected:
 
 	Texture &operator=(Texture &&) noexcept = default;
 
-private:
-	SizeType size{};
-	VectorType spacing{};
-	VectorType centrePosition{};
+  private:
+	SizeType size{};			 ///< The dimensions of the texture
+	VectorType spacing{};		 ///< The spacing between the values of the texture in world coordinates
+	VectorType centrePosition{}; ///< The position of the centre of the texture in world coordinates
 };
 
 } // namespace ExtensionTest
