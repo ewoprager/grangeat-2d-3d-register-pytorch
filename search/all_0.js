b0c0e1040fb57eec548492e66e42500a8357ea24
ewoprager <ewoprager@users.noreply.github.com>
2025-04-29 16:54:43 +0000

    deploy: 6d09a16d2fa0cd4684622b8d56f1e175fb56f346

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['2d_0',['2D',['../md__2home_2runner_2work_2grangeat-2d-3d-register-pytorch_2grangeat-2d-3d-register-pytorch_2Extension_2Conventions.html#autotoc_md7',1,'']]],
+  ['2d_0',['2D',['../conventions.html#autotoc_md7',1,'']]],
   ['2d_203d_20image_20registration_1',['Grangeat-based 2D/3D image registration',['../index.html',1,'']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['polar_20coordinates_0',['Polar coordinates',['../md__2home_2runner_2work_2grangeat-2d-3d-register-pytorch_2grangeat-2d-3d-register-pytorch_2Extension_2Conventions.html#autotoc_md9',1,'']]],
+  ['polar_20coordinates_0',['Polar coordinates',['../conventions.html#autotoc_md9',1,'']]],
   ['pybind11_5fmodule_1',['PYBIND11_MODULE',['../namespaceExtensionTest.html#a9b2c1fcd08722248990ac388deed597b',1,'ExtensionTest']]]
 ];
