c972f513df202413fa581c86ebb510c667d1a373
Edmund Prager <pragere12@gmail.com>
2025-06-12 19:42:04 +0100

    dev sinogram script can output file for tex pgf plotting of u and v padding

--- registration/lib/dev_scripts/dev_sinogram.py
+++ registration/lib/dev_scripts/dev_sinogram.py
@@ -1,6 +1,7 @@
 import argparse
 import logging.config
 
+import pathlib
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,86 +15,141 @@ from registration import pre_computed
 def map_back_and_forth():
     n_side: int = 5
 
-    _phi = LinearRange(-0.5 * torch.pi, 0.5 * torch.pi).generate_tex_coord_grid(40)
-    _theta = LinearRange(-0.5 * torch.pi, 0.5 * torch.pi).generate_tex_coord_grid(40)
-    _phi, _theta = torch.meshgrid(_phi, _theta)
-    _grid = Sinogram3dGrid(phi=_phi, theta=_theta, r=torch.zeros_like(_phi))
+    phi = LinearRange(-0.5 * torch.pi, 0.5 * torch.pi).generate_tex_coord_grid(40)
+    theta = LinearRange(-0.5 * torch.pi, 0.5 * torch.pi).generate_tex_coord_grid(40)
+    phi, theta = torch.meshgrid(phi, theta)
+    _grid = Sinogram3dGrid(phi=phi, theta=theta, r=torch.zeros_like(phi))
 
-    _u, _v, _r = sinogram.SinogramHEALPix.spherical_to_tex_coord(_grid, n_side)
+    u, v, r = sinogram.SinogramHEALPix.spherical_to_tex_coord(_grid, n_side)
 
-    print("n_side = {}".format(n_side))
-    print("desired u range: 0.5 to {}".format(3.0 * n_side + 2.5))
-    print("u range: {} to {}".format(_u.min().item(), _u.max().item()))
-    print("desired v range: 0.5 to {}".format(2.0 * n_side + 2.5))
-    print("v range: {} to {}".format(_v.min().item(), _v.max().item()))
+    logger.info("n_side = {}".format(n_side))
+    logger.info("desired u range: 0.5 to {}".format(3.0 * n_side + 2.5))
+    logger.info("u range: {} to {}".format(u.min().item(), u.max().item()))
+    logger.info("desired v range: 0.5 to {}".format(2.0 * n_side + 2.5))
+    logger.info("v range: {} to {}".format(v.min().item(), v.max().item()))
 
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(_phi.numpy(), _theta.numpy(), _u.numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("phi")
-    _axes.set_ylabel("theta")
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("u")
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(phi.numpy(), theta.numpy(), u.numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("phi")
+    axes.set_ylabel("theta")
+    axes.set_aspect('equal', 'box')
+    axes.set_title("u")
 
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(_phi.numpy(), _theta.numpy(), _v.numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("phi")
-    _axes.set_ylabel("theta")
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("v")
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(phi.numpy(), theta.numpy(), v.numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("phi")
+    axes.set_ylabel("theta")
+    axes.set_aspect('equal', 'box')
+    axes.set_title("v")
 
-    _phi2, _theta2 = sinogram.SinogramHEALPix.tex_coord_to_spherical(_u, _v, _r, n_side)
+    phi2, theta2 = sinogram.SinogramHEALPix.tex_coord_to_spherical(u, v, r, n_side)
 
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(_phi.numpy(), _theta.numpy(), _phi2.numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("phi")
-    _axes.set_ylabel("theta")
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("phi")
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(phi.numpy(), theta.numpy(), phi2.numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("phi")
+    axes.set_ylabel("theta")
+    axes.set_aspect('equal', 'box')
+    axes.set_title("phi")
 
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(_phi.numpy(), _theta.numpy(), _theta2.numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("phi")
-    _axes.set_ylabel("theta")
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("theta")
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(phi.numpy(), theta.numpy(), theta2.numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("phi")
+    axes.set_ylabel("theta")
+    axes.set_aspect('equal', 'box')
+    axes.set_title("theta")
 
     plt.show()
 
 
-def show_padding():
+def show_padding(output_directory: str | None):
     n_side: int = 5
 
-    _u = torch.arange(3 * n_side)
-    _v = torch.arange(2 * n_side)
-    _v, _u = torch.meshgrid(_v, _u)
+    u = torch.arange(3 * n_side)
+    v = torch.arange(2 * n_side)
+    v, u = torch.meshgrid(v, u)
 
-    _s = sinogram.SinogramHEALPix(_u.unsqueeze(0), LinearRange(0.0, 1.0))
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(torch.arange(3 * n_side + 4).numpy(), torch.arange(2 * n_side + 4).numpy(),
-                             _s.data[0].numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("u")
-    _axes.set_ylabel("v")
-    _axes.invert_yaxis()
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("u")
+    u_sinogram = sinogram.SinogramHEALPix(u.unsqueeze(0) + 1.0, LinearRange(0.0, 1.0))
+    u_padding = u_sinogram.data[0] < 0.5
+    u_sinogram._data -= 1.0
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(torch.arange(3 * n_side + 4).numpy(), torch.arange(2 * n_side + 4).numpy(),
+                           u_sinogram.data[0].numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("u")
+    axes.set_ylabel("v")
+    axes.invert_yaxis()
+    axes.set_aspect('equal', 'box')
+    axes.set_title("u")
 
-    _s = sinogram.SinogramHEALPix(_v.unsqueeze(0), LinearRange(0.0, 1.0))
-    _, _axes = plt.subplots()
-    _mesh = _axes.pcolormesh(torch.arange(3 * n_side + 4).numpy(), torch.arange(2 * n_side + 4).numpy(),
-                             _s.data[0].numpy())
-    plt.colorbar(_mesh)
-    _axes.set_xlabel("u")
-    _axes.set_ylabel("v")
-    _axes.invert_yaxis()
-    _axes.set_aspect('equal', 'box')
-    _axes.set_title("v")
+    v_sinogram = sinogram.SinogramHEALPix(v.unsqueeze(0) + 1.0, LinearRange(0.0, 1.0))
+    v_padding = v_sinogram.data[0] < 0.5
+    v_sinogram._data -= 1.0
+    _, axes = plt.subplots()
+    mesh = axes.pcolormesh(torch.arange(3 * n_side + 4).numpy(), torch.arange(2 * n_side + 4).numpy(),
+                           v_sinogram.data[0].numpy())
+    plt.colorbar(mesh)
+    axes.set_xlabel("u")
+    axes.set_ylabel("v")
+    axes.invert_yaxis()
+    axes.set_aspect('equal', 'box')
+    axes.set_title("v")
 
     plt.show()
+
+    if output_directory is not None:
+        output_directory = pathlib.Path(output_directory)
+        if not output_directory.is_dir():
+            logger.error("Output directory '{}' does not exist.".format(str(output_directory)))
+            exit(1)
+
+        i = torch.arange(3 * n_side + 4)
+        j = torch.arange(2 * n_side + 4)
+        j, i = torch.meshgrid(j, i)
+        u = u_sinogram.data[0].to(dtype=torch.int32)
+        v = v_sinogram.data[0].to(dtype=torch.int32)
+
+        # saving the scalar data in PGF plot table format
+        u_plot_path = output_directory / "u_padding_plot.dat"
+        with open(u_plot_path, "w") as file:
+            for row in range(i.size()[0]):
+                for col in range(i.size()[1]):
+                    file.write("{} {} {}\n".format(i[row, col], j[row, col], u[i.size()[0] - row - 1, col]))
+                file.write("\n")
+        logger.info("`u` coord padding plot saved to '{}'".format(str(u_plot_path)))
+
+        v_plot_path = output_directory / "v_padding_plot.dat"
+        with open(v_plot_path, "w") as file:
+            for row in range(i.size()[0]):
+                for col in range(i.size()[1]):
+                    file.write("{} {} {}\n".format(i[row, col], j[row, col], v[i.size()[0] - row - 1, col]))
+                file.write("\n")
+        logger.info("`v` coord padding plot saved to '{}'".format(str(u_plot_path)))
+
+        u_labels_path = output_directory / "u_padding_labels.tex"
+        with open(u_labels_path, "w") as file:
+            for row in range(i.size()[0]):
+                for col in range(i.size()[1]):
+                    file.write("\\node at (axis cs:{},{}) {{\\small {}}};\n".format(i[row, col], j[row, col],
+                                                                                    "-" if u_padding[
+                                                                                        i.size()[0] - row - 1, col] else
+                                                                                    u[i.size()[0] - row - 1, col]))
+                file.write("\n")
+        logger.info("`u` coord labels saved to '{}'".format(str(u_labels_path)))
+
+        v_labels_path = output_directory / "v_padding_labels.tex"
+        with open(v_labels_path, "w") as file:
+            for row in range(i.size()[0]):
+                for col in range(i.size()[1]):
+                    file.write("\\node at (axis cs:{},{}) {{\\small {}}};\n".format(i[row, col], j[row, col],
+                                                                                    "-" if v_padding[
+                                                                                        i.size()[0] - row - 1, col] else
+                                                                                    v[i.size()[0] - row - 1, col]))
+                file.write("\n")
+        logger.info("`v` coord labels saved to '{}'".format(str(v_labels_path)))
 
 
 def plot_sphere():
@@ -108,8 +164,8 @@ def plot_sphere():
     phi = torch.atan2(y, x)
     theta = (z / points.norm(dim=1)).asin()
 
-    print("theta in ({}, {})".format(theta.min(), theta.max()))
-    print("phi in ({}, {})".format(phi.min(), phi.max()))
+    logger.info("theta in ({}, {})".format(theta.min(), theta.max()))
+    logger.info("phi in ({}, {})".format(phi.min(), phi.max()))
 
     grid = Sinogram3dGrid(phi=phi, theta=theta, r=20.0 * torch.ones_like(phi)).unflip()
 
@@ -147,12 +203,13 @@ if __name__ == "__main__":
     _subparsers = _parser.add_subparsers(dest="command")
     _parser_map = _subparsers.add_parser("map-back-and-forth", help="")
     _parser_pad = _subparsers.add_parser("show-padding", help="")
+    _parser_pad.add_argument("-o", "--output-dir", type=str, help="")
     _parser_run = _subparsers.add_parser("plot-sphere", help="")
     _args = _parser.parse_args()
 
     if _args.command == "map-back-and-forth":
         map_back_and_forth()
     elif _args.command == "show-padding":
-        show_padding()
+        show_padding(output_directory=_args.output_dir)
     else:  # _args.command == "plot-sphere"
         plot_sphere()
