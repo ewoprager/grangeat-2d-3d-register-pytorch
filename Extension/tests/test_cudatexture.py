e29fc4c746b627a282362f2a5fff28535a40ddb1
ewoprager <ep585@cam.ac.uk>
2025-06-18 12:12:25 +0100

    test fix

--- Extension/tests/test_cudatexture.py
+++ Extension/tests/test_cudatexture.py
@@ -2,39 +2,37 @@ import pytest
 
 import torch
 
-from Extension import reg23
-from Extension import resample_sinogram3d_cuda_texture
+if torch.cuda.is_available():
+
+    from Extension import reg23
+    from Extension import resample_sinogram3d_cuda_texture
 
 
-def test_cuda_texture():
-    if not torch.cuda.is_available():
-        return
-    device = torch.device("cuda")
+    def test_cuda_texture():
+        device = torch.device("cuda")
 
-    d2 = torch.tensor([[1, 2], [3, 4]], device=device, dtype=torch.float32)
+        d2 = torch.tensor([[1, 2], [3, 4]], device=device, dtype=torch.float32)
 
-    reg23.CUDATexture2D(d2, "zero", "zero")
+        reg23.CUDATexture2D(d2, "zero", "zero")
 
-    d3 = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], device=device, dtype=torch.float32)
-    reg23.CUDATexture3D(d3, "zero", "zero", "zero")
+        d3 = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], device=device, dtype=torch.float32)
+        reg23.CUDATexture3D(d3, "zero", "zero", "zero")
 
-    with pytest.raises(TypeError):
-        reg23.CUDATexture3D(d3, "zero", "zero")
+        with pytest.raises(TypeError):
+            reg23.CUDATexture3D(d3, "zero", "zero")
 
-    with pytest.raises(RuntimeError):
-        reg23.CUDATexture3D(d2, "zero", "zero", "zero")
+        with pytest.raises(RuntimeError):
+            reg23.CUDATexture3D(d2, "zero", "zero", "zero")
 
-    with pytest.raises(RuntimeError):
-        reg23.CUDATexture3D(d3.to(device=torch.device("cpu")), "zero", "zero", "zero")
+        with pytest.raises(RuntimeError):
+            reg23.CUDATexture3D(d3.to(device=torch.device("cpu")), "zero", "zero", "zero")
 
 
-def test_resampling():
-    if not torch.cuda.is_available():
-        return
-    device = torch.device("cuda")
+    def test_resampling():
+        device = torch.device("cuda")
 
-    d3 = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], device=device, dtype=torch.float32)
-    texture = reg23.CUDATexture3D(d3, "zero", "zero", "zero")
-    res = resample_sinogram3d_cuda_texture(
-        texture, "classic", 0.1, torch.eye(4, device=device), torch.zeros(1, device=device),
-        torch.zeros(1, device=device))
+        d3 = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], device=device, dtype=torch.float32)
+        texture = reg23.CUDATexture3D(d3, "zero", "zero", "zero")
+        res = resample_sinogram3d_cuda_texture(
+            texture, "classic", 0.1, torch.eye(4, device=device), torch.zeros(1, device=device),
+            torch.zeros(1, device=device))
