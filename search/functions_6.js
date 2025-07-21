480b27886019918473657793b63faa7a14c5a84a
ewoprager <ewoprager@users.noreply.github.com>
2025-07-21 14:39:48 +0000

    deploy: 5c3d2f285ec5a304f08cd966a25ba230dfc3c409

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['handle_0',['handle',['../classreg23_1_1CUDATexture2D.html#a04d9c4cf505f61ee95db3f572edf53bf',1,'reg23::CUDATexture2D::Handle()'],['../classreg23_1_1CUDATexture3D.html#a3cba07dbcba4c85df5eb3671e32489db',1,'reg23::CUDATexture3D::Handle()']]]
+  ['handle_0',['handle',['../classreg23_1_1CUDATexture2D.html#a32676e0842574a517dc4c3ab5286d464',1,'reg23::CUDATexture2D::Handle()'],['../classreg23_1_1CUDATexture3D.html#aa7f71acfbc84bdfbc25e5a480aff7042',1,'reg23::CUDATexture3D::Handle()']]]
 ];
