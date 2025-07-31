69a9a69536b37579ede7cd4ebd585eb90eb8c005
ewoprager <ewoprager@users.noreply.github.com>
2025-07-31 11:03:34 +0000

    deploy: 74cb188479987baca23cfdc3bbe98e93da258857

--- search/all_6.js
+++ search/all_6.js
@@ -3,7 +3,7 @@ var searchData=
   ['data_20structures_0',['Data Structures',['../group__data__structures.html',1,'']]],
   ['dehomogenise_1',['DeHomogenise',['../classreg23_1_1Vec.html#a593bdad9c83e9a10b2ce3652262a061b',1,'reg23::Vec']]],
   ['detectorspacing_2',['detectorSpacing',['../structreg23_1_1ProjectDRR_1_1CommonData.html#a68ee18cb8cb9467d643aab81e5e1b7cb',1,'reg23::ProjectDRR::CommonData']]],
-  ['dimensionality_3',['dimensionality',['../classreg23_1_1Texture.html#a140ce47d5266c4e997daf8dfc25a3103',1,'reg23::Texture::DIMENSIONALITY'],['../classreg23_1_1Vec.html#a553a7d2533d441c99d839d34dcd6ab05',1,'reg23::Vec::dimensionality']]],
+  ['dimensionality_3',['dimensionality',['../classreg23_1_1Vec.html#a553a7d2533d441c99d839d34dcd6ab05',1,'reg23::Vec::dimensionality'],['../classreg23_1_1Texture.html#a140ce47d5266c4e997daf8dfc25a3103',1,'reg23::Texture::DIMENSIONALITY']]],
   ['dintegrateloopeddmappingparameter_4',['dintegrateloopeddmappingparameter',['../structreg23_1_1Radon2D.html#a7c50d62fe6c821109f35ab177449a560',1,'reg23::Radon2D::DIntegrateLoopedDMappingParameter()'],['../structreg23_1_1Radon3D.html#a1242c04f4665948c0629d39dbf626f3b',1,'reg23::Radon3D::DIntegrateLoopedDMappingParameter()']]],
   ['dradon2ddr_5fcpu_5',['DRadon2DDR_CPU',['../group__pytorch__functions.html#gafe0a581bb38e5ddeed2f237ca5e31c5c',1,'reg23']]],
   ['dradon2ddr_5fcuda_6',['DRadon2DDR_CUDA',['../group__pytorch__functions.html#ga33206dc5e7caf417cf653f32f7158d59',1,'reg23']]],
--- search/all_d.js
+++ search/all_d.js
@@ -5,6 +5,9 @@ var searchData=
   ['mappingindextooffset_2',['mappingindextooffset',['../structreg23_1_1Radon2D_1_1CommonData.html#a870cb04502228d8d0a5671b3e9049982',1,'reg23::Radon2D::CommonData::mappingIndexToOffset'],['../structreg23_1_1Radon3D_1_1CommonData.html#acc89c99eec3491b1f53a53008b7bf7ca',1,'reg23::Radon3D::CommonData::mappingIndexToOffset']]],
   ['mappingworldtotexcoord_3',['MappingWorldToTexCoord',['../classreg23_1_1Texture.html#ab2bad495de620b2fc68f1cf3722780ed',1,'reg23::Texture']]],
   ['matmul_4',['MatMul',['../group__data__structures.html#gaed0b516ddbb4192d9e81ceda8f730f7b',1,'reg23']]],
-  ['modes_5',['Address modes',['../group__pytorch__functions.html#autotoc_md0',1,'']]],
-  ['modulo_6',['Modulo',['../group__general__tools.html#ga4e29f108e1aff3e20801f902034a8e94',1,'reg23']]]
+  ['max_5',['Max',['../classreg23_1_1Vec.html#a2d73a5cc0dbdd9576b781f55562c5d52',1,'reg23::Vec']]],
+  ['min_6',['Min',['../classreg23_1_1Vec.html#abd587a3a20d1171f827a459be650489e',1,'reg23::Vec']]],
+  ['minmax_7',['MinMax',['../classreg23_1_1Vec.html#aad1cfb9a0123e1d7d697173445b12345',1,'reg23::Vec']]],
+  ['modes_8',['Address modes',['../group__pytorch__functions.html#autotoc_md0',1,'']]],
+  ['modulo_9',['Modulo',['../group__general__tools.html#ga4e29f108e1aff3e20801f902034a8e94',1,'reg23']]]
 ];
