b4be38488c3bc6cb10f24903d92c5b6e96d4a1ee
ewoprager <ep585@cam.ac.uk>
2025-09-03 14:44:14 +0100

    added test_project_drr_cuboid_mask

--- Extension/tests/test_project_drr.py
+++ Extension/tests/test_project_drr.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from Extension import project_drr
+from Extension import project_drr, project_drr_cuboid_mask
 
 
 def test_project_drr():
@@ -50,3 +50,51 @@ def test_project_drr():
     offset = project_drr(input_, voxel_spacing, h_matrix_inv, source_distance, output_size[1], output_size[0],
                          3 * detector_spacing.to(dtype=torch.float64), detector_spacing)
     assert zero[3:, 3:] == pytest.approx(offset[:-3, :-3])
+
+
+def test_project_drr_cuboid_mask():
+    input_size = torch.tensor([8, 12, 11])
+    voxel_spacing = torch.tensor([0.1, 0.2, 0.3])
+    h_matrix_inv = torch.eye(4)
+    source_distance = 1000.0
+    output_size = torch.Size([10, 15])
+    detector_spacing = torch.tensor([0.2, 0.25])
+    res = project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                  output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+    assert res.size() == output_size
+    if torch.cuda.is_available():
+        res_cuda = project_drr_cuboid_mask(input_size.cuda(), voxel_spacing.cuda(), h_matrix_inv.cuda(),
+                                           source_distance, output_size[1], output_size[0],
+                                           torch.zeros(2, dtype=torch.float64).cuda(), detector_spacing.cuda())
+        assert res == pytest.approx(res_cuda.cpu(), abs=0.01)
+
+    # input must be 3D, so these should raise a runtime error
+    input_size = torch.tensor([11, 12, 8, 5])
+    with pytest.raises(RuntimeError):
+        project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+    input_size = torch.tensor([11, 12])
+    with pytest.raises(RuntimeError):
+        project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+
+    # voxel spacing and detector spacing must be 1D and of lengths 3 & 2 respectively, so these should raise a runtime
+    # error
+    input_size = torch.tensor([8, 12, 11])
+    voxel_spacing = torch.tensor([0.2, 0.1])
+    with pytest.raises(RuntimeError):
+        project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+    voxel_spacing = torch.tensor([0.1, 0.2, 0.3])
+    detector_spacing = torch.tensor([0.1, 0.2, 0.3])
+    with pytest.raises(RuntimeError):
+        project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+
+    # output offset should shift the image
+    detector_spacing = torch.tensor([0.2, 0.25])
+    zero = project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                   output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+    offset = project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
+                                     output_size[0], 3 * detector_spacing.to(dtype=torch.float64), detector_spacing)
+    assert zero[3:, 3:] == pytest.approx(offset[:-3, :-3], abs=0.001)
