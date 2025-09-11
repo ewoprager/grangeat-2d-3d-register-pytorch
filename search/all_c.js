a8c6eb414ff9de2aee58be7d597a38ab7ef691b0
ewoprager <ewoprager@users.noreply.github.com>
2025-09-11 15:04:36 +0000

    deploy: 5d598447efe72bbc0ef5bf183c4cd7bf4b1222d2

--- search/all_c.js
+++ search/all_c.js
@@ -4,5 +4,6 @@ var searchData=
   ['length_1',['Length',['../classreg23_1_1Vec.html#a84a87411bcc96f2c6406e74381b30d6d',1,'reg23::Vec']]],
   ['linear_2',['Linear',['../structreg23_1_1Linear.html',1,'reg23']]],
   ['linear2_3',['Linear2',['../structreg23_1_1Linear2.html',1,'reg23']]],
-  ['linear_3c_20vectortype_20_3e_4',['Linear&lt; VectorType &gt;',['../structreg23_1_1Linear.html',1,'reg23']]]
+  ['linear_3c_20vectortype_20_3e_4',['Linear&lt; VectorType &gt;',['../structreg23_1_1Linear.html',1,'reg23']]],
+  ['localncc_5fcpu_5',['LocalNCC_CPU',['../namespacereg23.html#ae36d16c848eb18be62efc9c62e3cd8ea',1,'reg23']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['length_0',['Length',['../classreg23_1_1Vec.html#a84a87411bcc96f2c6406e74381b30d6d',1,'reg23::Vec']]]
+  ['length_0',['Length',['../classreg23_1_1Vec.html#a84a87411bcc96f2c6406e74381b30d6d',1,'reg23::Vec']]],
+  ['localncc_5fcpu_1',['LocalNCC_CPU',['../namespacereg23.html#ae36d16c848eb18be62efc9c62e3cd8ea',1,'reg23']]]
 ];
