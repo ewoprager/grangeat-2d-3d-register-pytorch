acf35098e50c984fcb2433edcf8787c37dcfdd65
ewoprager <ep585@cam.ac.uk>
2025-09-30 14:20:06 +0100

    sandbxoign

--- registration/lib/plot.py
+++ registration/lib/plot.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from typing import NamedTuple, Optional
+
 import numpy as np
 import torch
 import pyvista as pv
@@ -66,3 +69,151 @@ def visualise_planes_as_points(grid: Sinogram3dGrid, scalars: torch.Tensor | Non
     else:
         pl.add_points(points.cpu().numpy(), scalars=scalars.flatten().cpu().numpy())
     pl.show()
+
+
+class Series(NamedTuple):
+    ys: torch.Tensor
+    intersects_origin: bool
+    origin_y_offset: float = 0.0
+
+
+class DataFit(ABC):
+    @abstractmethod
+    def text_equation(self) -> str:
+        pass
+
+    @abstractmethod
+    def generate_ys(self, xs: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def generate_and_plot_ys(self, *, axes, xs: torch.Tensor, label_prefix: str, **kwargs) -> None:
+        pass
+
+
+class LinearFit(DataFit):
+    def __init__(self, *, xs: torch.Tensor, series: Series):
+        self._xs = xs
+        self._series = series
+        ys_fit = series.ys - self._series.origin_y_offset
+        self._coefficients = torch_polyfit(xs, ys_fit, degree=1)
+
+    @property
+    def xs(self) -> torch.Tensor:
+        return self._xs
+
+    @property
+    def series(self) -> Series:
+        return self._series
+
+    @property
+    def coefficients(self) -> torch.Tensor:
+        return self._coefficients
+
+    def text_equation(self) -> str:
+        return "$y = {}x {}$".format(to_latex_scientific(self.coefficients[1].item()),
+                                     to_latex_scientific(self.coefficients[0].item(), include_plus=True))
+
+    def generate_ys(self, xs: torch.Tensor) -> torch.Tensor:
+        return self.coefficients[0] + self.coefficients[1] * xs + self._series.origin_y_offset
+
+    def generate_and_plot_ys(self, *, axes, xs: torch.Tensor, label_prefix: str, **kwargs) -> None:
+        ys_fit = self.coefficients[0] + self.coefficients[1] * xs + self._series.origin_y_offset
+        axes.plot(xs.cpu().numpy(), ys_fit.cpu().numpy(), label="{}: {}".format(label_prefix, self.text_equation()),
+                  **kwargs)
+
+    def scatter(self, axes, **kwargs) -> None:
+        axes.scatter(self.xs, self.series.ys, **kwargs)
+
+    def correlation_coefficient(self) -> torch.Tensor:
+        return torch.corrcoef(torch.stack((self.xs.flatten(), self.series.ys.flatten()), dim=0))[0, 1]
+
+
+class QuadraticFit(DataFit):
+    def __init__(self, *, xs: torch.Tensor, series: Series):
+        self._xs = xs
+        self._series = series
+        ys_fit = series.ys - self._series.origin_y_offset
+        self._coefficients = torch_polyfit(xs, ys_fit, degree=2)
+
+    @property
+    def xs(self) -> torch.Tensor:
+        return self._xs
+
+    @property
+    def series(self) -> Series:
+        return self._series
+
+    @property
+    def coefficients(self) -> torch.Tensor:
+        return self._coefficients
+
+    def text_equation(self) -> str:
+        return "$y = {}x^2 {}x {}$".format(to_latex_scientific(self.coefficients[2].item()),
+                                           to_latex_scientific(self.coefficients[1].item(), include_plus=True),
+                                           to_latex_scientific(self.coefficients[0].item(), include_plus=True))
+
+    def generate_ys(self, xs: torch.Tensor) -> torch.Tensor:
+        return self.coefficients[0] + self.coefficients[1] * xs + self.coefficients[
+            2] * xs * xs + self._series.origin_y_offset
+
+    def generate_and_plot_ys(self, *, axes, xs: torch.Tensor, label_prefix: str, **kwargs) -> None:
+        ys_fit = self.coefficients[0] + self.coefficients[1] * xs + self.coefficients[
+            2] * xs * xs + self._series.origin_y_offset
+        axes.plot(xs.cpu().numpy(), ys_fit.cpu().numpy(), label="{}: {}".format(label_prefix, self.text_equation()),
+                  **kwargs)
+
+    def scatter(self, axes, **kwargs) -> None:
+        axes.scatter(self.xs, self.series.ys, **kwargs)
+
+
+class PowerFit(DataFit):
+    def __init__(self, *, xs: torch.Tensor, series: Series, linear_fit_log_log: LinearFit):
+        self._xs = xs
+        self._series = series
+        self._linear_fit_log_log = linear_fit_log_log
+
+    @staticmethod
+    def build(*, xs: torch.Tensor, series: Series) -> Optional['PowerFit']:
+        xs_fit = xs[1:] if series.intersects_origin else xs
+        ys_fit = (series.ys[1:] if series.intersects_origin else series.ys) - series.origin_y_offset
+        if not ((xs_fit > 0.0).all() and (ys_fit > 0.0).all()):
+            return None
+        return PowerFit(xs=xs, series=series, linear_fit_log_log=LinearFit(  #
+            xs=xs_fit.log(), series=Series(ys_fit.log(), intersects_origin=False, origin_y_offset=0.0)))
+
+    @property
+    def xs(self) -> torch.Tensor:
+        return self._xs
+
+    @property
+    def series(self) -> Series:
+        return self._series
+
+    @property
+    def coefficients(self) -> torch.Tensor:
+        return torch.tensor([self._linear_fit_log_log.coefficients[0].exp(), self._linear_fit_log_log.coefficients[1]])
+
+    def text_equation(self) -> str:
+        return "$y = {}x^{{{}}}$".format(to_latex_scientific(self.coefficients[0].item()),
+                                         to_latex_scientific(self.coefficients[1].item()))
+
+    def generate_ys(self, xs: torch.Tensor) -> torch.Tensor:
+        xs_fit = xs[1:] if self._series.intersects_origin else xs
+        return self.coefficients[0] * xs_fit.pow(self.coefficients[1]) + self._series.origin_y_offset
+
+    def generate_and_plot_ys(self, *, axes, xs: torch.Tensor, label_prefix: str, **kwargs) -> None:
+        xs_fit = xs[1:] if self._series.intersects_origin else xs
+        ys_fit = self.coefficients[0] * xs_fit.pow(self.coefficients[1]) + self._series.origin_y_offset
+        axes.plot(xs_fit.cpu().numpy(), ys_fit.cpu().numpy(), label="{}: {}".format(label_prefix, self.text_equation()),
+                  **kwargs)
+
+    def scatter_log_log(self, axes, **kwargs) -> None:
+        self._linear_fit_log_log.scatter(axes, **kwargs)
+
+    def plot_fit_log_log(self, axes, label_prefix: str) -> None:
+        self._linear_fit_log_log.generate_and_plot_ys(axes=axes, xs=self._linear_fit_log_log.xs,
+                                                      label_prefix=label_prefix)
+
+    def log_log_correlation_coefficient(self) -> torch.Tensor:
+        return self._linear_fit_log_log.correlation_coefficient()
--- scripts/truncation_theory_sandbox.py
+++ scripts/truncation_theory_sandbox.py
@@ -1,21 +1,30 @@
+from typing import Tuple
+
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+
+from tqdm import tqdm
+from registration.lib.plot import Series, LinearFit, PowerFit, QuadraticFit
 
 
 def tests():
     N = 100_000
-    sigma = 1.0
-    points = np.random.multivariate_normal(mean=np.array([1.0, 0.0]), cov=np.array([[sigma, 0.0], [0.0, sigma]]),
-                                           size=N)
+    sigma = 0.001
+    dimensionality = 100
+    mean = np.zeros(dimensionality)
+    mean[0] = 1.0
+    points = np.random.multivariate_normal(mean=mean, cov=sigma * np.eye(dimensionality),
+                                           size=N)  # size = (N, dimensionality)
 
     if False:
         # scatter plot
         plt.scatter(points[:, 0], points[:, 1])
         plt.show()
 
-    magnitudes = np.sqrt((points * points).sum(axis=1))
+    magnitudes = np.sqrt((points * points).sum(axis=1))  # size = (N,)
 
-    if False:
+    if True:
         # histogram of magnitudes
         plt.hist(magnitudes, bins=100)
         plt.show()
@@ -31,32 +40,71 @@ def tests():
     print("Mean cosine value = ", mean)
 
 
-def avg_cosine_at_dist(*, distance: float, sigma: float, sample_count: int = 100_000) -> float:
-    points = np.random.multivariate_normal(mean=np.array([distance, 0.0]), cov=np.array([[sigma, 0.0], [0.0, sigma]]),
-                                           size=sample_count)
-    magnitudes = np.sqrt((points * points).sum(axis=1))
-    cosines = points[:, 0] / magnitudes
+def avg_cosine_at_dist(*, distance: float, sigma: float, sample_count: int = 1000, dimensionality: int = 300) -> float:
+    mean = np.zeros(dimensionality)
+    mean[0] = distance
+    points = np.random.multivariate_normal(mean=mean, cov=sigma * np.eye(dimensionality),
+                                           size=sample_count)  # size = (sample_count, dimensionality)
+    magnitudes = np.sqrt((points * points).sum(axis=1))  # size = (sample_count,)
+    cosines = points[:, 0] / magnitudes  # size = (sample_count,)
     return cosines.mean()
 
 
-def main():
-    N = 100
-    sigma_maxes = np.linspace(0.1, 10, 20)
+def sigma_function_peak(sigma_max: float, n: int) -> np.ndarray:
+    return np.concat((np.linspace(0.0, sigma_max, n // 2, endpoint=False), np.linspace(sigma_max, 0.0, n - (n // 2))))
 
-    colors = np.stack([np.linspace(0.0, 1.0, len(sigma_maxes)), np.linspace(1.0, 0.0, len(sigma_maxes)),
-                       np.zeros(len(sigma_maxes))])
+
+def sigma_function_flat(sigma_max: float, central_fraction: float, n: int) -> np.ndarray:
+    ramp_n = int(np.floor(0.5 * (1.0 - central_fraction) * float(n)))
+    return np.concat((np.linspace(0.0, sigma_max, ramp_n, endpoint=False), np.full(n - 2 * ramp_n - 1, sigma_max),
+                      np.linspace(sigma_max, 0.0, ramp_n + 1)))
+
+
+def sigma_function_cosine(sigma_max: float, n: int) -> np.ndarray:
+    return sigma_max * np.cos(np.linspace(-0.5 * np.pi, 0.5 * np.pi, n))
+
+
+def main1():
+    N = 60
+    sigma_maxes = np.geomspace(0.00001, 0.01, 10)
+
+    colors = np.stack(
+        [np.linspace(0.0, 1.0, len(sigma_maxes)), np.linspace(1.0, 0.0, len(sigma_maxes)), np.zeros(len(sigma_maxes))])
     colors = [(r.item(), g.item(), b.item()) for r, g, b in zip(colors[0, :], colors[1, :], colors[2, :])]
 
-    distances = np.linspace(0.0, 5, N)
-    for j in range(len(sigma_maxes)):
+    fig, axes = plt.subplots(1, 2)
+
+    distances = np.linspace(1.0, 0.0, N)
+    truncation_fractions = 1.0 - distances
+    for j in tqdm(range(len(sigma_maxes))):
         avg_cosines = np.zeros(len(distances))
-        sigmas = np.concat(
-            (np.linspace(0.0, sigma_maxes[j], N // 2, endpoint=False), np.linspace(sigma_maxes[j], 0.0, N - (N // 2))))
+        # sigmas = sigma_function_peak(sigma_maxes[j].item(), N)
+        # sigmas = sigma_function_flat(sigma_maxes[j].item(), 0.95, N)
+        sigmas = sigma_function_cosine(sigma_maxes[j].item(), N)
+        axes[0].plot(truncation_fractions, sigmas, color=colors[j])
         for i in range(len(distances)):
             avg_cosines[i] = avg_cosine_at_dist(distance=distances[i].item(), sigma=sigmas[i].item())
-        plt.plot(distances, avg_cosines, color=colors[j])
+        axes[1].plot(truncation_fractions, -avg_cosines, color=colors[j])
+
+        fit_ys = -torch.tensor(avg_cosines)[:(N // 2)]
+        fit_xs = torch.tensor(truncation_fractions)[:(N // 2)]
+        valid = (fit_ys + 1.0) > 0.0
+        fit_ys = fit_ys[valid]
+        fit_xs = fit_xs[valid]
+        # power_fit = PowerFit.build(xs=fit_xs, series=Series(ys=fit_ys, intersects_origin=False, origin_y_offset=-1.0))
+        # if power_fit is not None:
+        #     power_fit.generate_and_plot_ys(axes=axes[1], xs=fit_xs, label_prefix="Power fit", color=colors[j],
+        #                                    linestyle="--")
+        quadratic_fit = QuadraticFit(xs = fit_xs, series=Series(ys=fit_ys, intersects_origin=False, origin_y_offset=-1.0))
+        quadratic_fit.generate_and_plot_ys(axes=axes[1], xs=fit_xs, label_prefix="Power fit", color=colors[j],
+                                           linestyle=":")
+
+    axes[0].set_xlabel("truncation fraction")
+    axes[0].set_ylabel("$\\sigma$")
+    axes[1].set_xlabel("truncation fraction")
+    axes[1].set_ylabel("-$\\cos \\theta$ average")
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main1()
