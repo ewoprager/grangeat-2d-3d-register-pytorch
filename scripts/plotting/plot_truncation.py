have face-on and top-down results. tried getting histograms / mean / variance of moving : fixed image intensity ratios, but a bit stumped really.

scripts/plotting/plot_truncation.py
@@ -96,7 +96,7 @@ def main(load_path: pathlib.Path, save_path: pathlib.Path):
 
         name = file.stem
 
-        if "vals_at_gt" in pdata:
+        if False and "vals_at_gt" in pdata:
             vals_at_gt = pdata["vals_at_gt"]
             analysis = Analysis(xs=truncation_fractions, ys=vals_at_gt, dependent_common_dim=1, intersects_origin=True,
                                 origin_y_offset=-1.0)
@@ -190,6 +190,46 @@ def main(load_path: pathlib.Path, save_path: pathlib.Path):
             axes.set_ylabel("obj. func. value at ground truth")
             axes.set_title("{}: distance in SE(3) between G.T. and optimum std. dev.".format(name))
             plt.legend()
+
+        if True and "pdf_means" in pdata:
+            fig, axes = plt.subplots()
+            pdf_means = pdata["pdf_means"].mean(axis=1)
+
+            xs = (1.0 - truncation_fractions).cpu().numpy()
+            ys = pdf_means.cpu().numpy()
+            axes.scatter(xs, ys)
+
+            linear_fit_ys = np.exp(ys) - 1.0
+            sum_xy = np.sum(xs * linear_fit_ys)
+            sum_x2 = np.sum(xs * xs)
+            m = sum_xy / sum_x2
+            fitted_ys = np.log(1.0 + m * xs)
+            axes.plot(xs, fitted_ys, label="fit: $y = \\log (1 + {}x)$".format(to_latex_scientific(m)))
+
+            axes.set_xlabel("1 - truncation fraction")
+            axes.set_ylabel("intensity ratio")
+            axes.set_title("{}: mean intensity ratio".format(name))
+            axes.legend()
+
+        if True and "pdf_stds" in pdata:
+            fig, axes = plt.subplots()
+            pdf_stds = pdata["pdf_stds"].mean(axis=1)
+
+            xs = (1.0 - truncation_fractions).cpu().numpy()
+            ys = pdf_stds.cpu().numpy()
+            axes.scatter(xs, ys)
+
+            linear_fit_ys = np.exp(ys) - 1.0
+            sum_xy = np.sum(xs * linear_fit_ys)
+            sum_x2 = np.sum(xs * xs)
+            m = sum_xy / sum_x2
+            fitted_ys = np.log(1.0 + m * xs)
+            axes.plot(xs, fitted_ys, label="fit: $y = \\log (1 + {}x)$".format(to_latex_scientific(m)))
+
+            axes.set_xlabel("1 - truncation fraction")
+            axes.set_ylabel("intensity ratio")
+            axes.set_title("{}: std. dev. intensity ratio".format(name))
+            axes.legend()
 
         if False:
             plt.show()

scripts/truncation.py
@@ -62,6 +62,7 @@ class RegistrationData:
 
         ct_volume, self._ct_spacing = data.load_volume(pathlib.Path(ct_path), downsample_factor=downsample_factor)
         ct_volume = ct_volume.to(device=device, dtype=torch.float32)
+        ct_volume = ct_volume - ct_volume.mean() # !!! shifting the volume to zero-mean, so now considering real, not non-negative
         self._ct_volumes = [ct_volume] + [truncate(ct_volume, fraction) for fraction in truncation_fractions]
         self._ct_spacing = self._ct_spacing.to(device=device)
 
@@ -451,7 +452,7 @@ class MeasureTask:
     def registration_data(self) -> RegistrationData:
         return self._registration_data
 
-    def run(self, run_name: str | None = None, get_distances: bool = True) -> None:
+    def run(self, run_name: str | None = None, get_distances: bool = False, get_pdf_params: bool = False) -> None:
         def obj_func(tensor: torch.Tensor) -> torch.Tensor:
             return RegistrationData.sim_metric_ncc(
                 *self.registration_data.images_drr(mapping_parameters_to_transformation(tensor)))
@@ -461,6 +462,9 @@ class MeasureTask:
         vals_at_gt = torch.zeros((fraction_count, self._repetition_count))
         if get_distances:
             opt_distances = torch.zeros((fraction_count, self._repetition_count))
+        if get_pdf_params:
+            pdf_means = torch.zeros((fraction_count, self._repetition_count))
+            pdf_stds = torch.zeros((fraction_count, self._repetition_count))
 
         for i in range(len(truncation_fractions)):
             first: bool = True
@@ -468,7 +472,7 @@ class MeasureTask:
             for j in range(self._repetition_count):
                 # new transformation GT
                 self.registration_data.transformation_gt = Transformation.random_gaussian(
-                    rotation_mean=torch.pi * torch.tensor([0.5, 0.0, 0.0], device=self.device), rotation_std=0.1,
+                    rotation_mean=torch.pi * torch.tensor([0.0, 0.0, 0.0], device=self.device), rotation_std=0.1,
                     translation_mean=torch.zeros(3, device=self.device), translation_std=0.0)
                 if self.registration_data.mask_transformation is not None:
                     self.registration_data.mask_transformation = self.registration_data.transformation_gt
@@ -494,18 +498,27 @@ class MeasureTask:
                         max_reductions=10))
                     opt_distances[i, j] = local_found.distance(
                         self.registration_data.transformation_gt.to(device=self.device))
+                if get_pdf_params:
+                    non_zero = torch.logical_and(moving_image > 1e-3, fixed_image > 1e-3)
+                    ratio_image = moving_image[non_zero].flatten() / fixed_image[non_zero].flatten()
+                    pdf_means[i, j] = ratio_image.mean()
+                    pdf_stds[i, j] = ratio_image.std()
+
                 first = False
 
         to_save = {"truncation_fractions": truncation_fractions, "vals_at_gt": vals_at_gt}
         if get_distances:
             to_save["opt_distances"] = opt_distances
+        if get_pdf_params:
+            to_save["pdf_means"] = pdf_means
+            to_save["pdf_stds"] = pdf_stds
 
         file_name = "{}.pkl".format(self._name) if run_name is None else "{}_{}.pkl".format(self._name, run_name)
         torch.save(to_save, MeasureTask.SAVE_DIRECTORY / file_name)
 
 
 def evaluate_and_save_landscape(*, cache_directory: str, ct_path: str, device, show: bool = False):
-    stop: float = 0.8
+    stop: float = 1.0
     step: float = 0.05
     truncation_fractions = [step * float(i) for i in range(1, int(round(stop / step)))]
     logger.info("Truncation fractions: {}".format(truncation_fractions))
@@ -524,14 +537,15 @@ def evaluate_and_save_landscape(*, cache_directory: str, ct_path: str, device, s
         #     task.run(images_function_name="drr", sim_metric=RegistrationData.sim_metric_ncc, show=show,
         #              name="{:.3f}".format(truncation_fractions[i]).split(".")[1])
 
-        measure_task = MeasureTask(registration_data, repetition_count=30, name="face_on_{}".format(downsample_factor),
+        measure_task = MeasureTask(registration_data, repetition_count=30, name="top_down_{}".format(downsample_factor),
                                    device=device, draw=False)
 
-        measure_task.run(get_distances=False)
+        measure_task.run(get_distances=False, get_pdf_params=True)
 
-        registration_data.mask_transformation = registration_data.transformation_gt
+        if False:
+            registration_data.mask_transformation = registration_data.transformation_gt
 
-        measure_task.run(run_name="masked_at_gt", get_distances=False)
+            measure_task.run(run_name="masked_at_gt", get_distances=False, get_histogram=True)
 
         plt.show()
 