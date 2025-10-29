tested; gives incorrect values

Extension/autograd_impl.py
@@ -10,8 +10,10 @@ class ProjectDRR(torch.autograd.Function):
                 detector_spacing: torch.Tensor) -> torch.Tensor:
         output = ops.project_drr(volume, voxel_spacing, homography_matrix_inverse, source_distance, output_width,
                                  output_height, output_offset, detector_spacing)
-        ctx.save_for_backwards(homography_matrix_inverse, volume, voxel_spacing, source_distance, output_width,
-                               output_height, output_offset, detector_spacing)
+        ctx.save_for_backward(homography_matrix_inverse, volume, voxel_spacing, output_offset, detector_spacing)
+        ctx.source_distance = source_distance
+        ctx.output_width = output_width
+        ctx.output_height = output_height
         return output
 
     @staticmethod
@@ -21,8 +23,12 @@ class ProjectDRR(torch.autograd.Function):
         :param d_loss_d_drr: tensor of size (drr_height, drr_width); the derivative of the loss w.r.t. the DRR
         :return: tensor of size (
         """
-        (homography_matrix_inverse, volume, voxel_spacing, source_distance, output_width, output_height, output_offset,
+        (homography_matrix_inverse, volume, voxel_spacing, output_offset,
          detector_spacing) = ctx.saved_tensors
+        source_distance = ctx.source_distance
+        output_width = ctx.output_width
+        output_height = ctx.output_height
+
 
         d_drr_d_hmi = ops.d_project_drr_d_hmi(volume, voxel_spacing, homography_matrix_inverse, source_distance,
                                               output_width, output_height, output_offset, detector_spacing)

Extension/tests/test_project_drr.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from Extension import project_drr, project_drr_cuboid_mask
+from Extension import project_drr, project_drr_cuboid_mask, autograd
 
 
 def test_project_drr():
@@ -98,3 +98,38 @@ def test_project_drr_cuboid_mask():
     offset = project_drr_cuboid_mask(input_size, voxel_spacing, h_matrix_inv, source_distance, output_size[1],
                                      output_size[0], 3 * detector_spacing.to(dtype=torch.float64), detector_spacing)
     assert zero[3:, 3:] == pytest.approx(offset[:-3, :-3], abs=0.001)
+
+
+import matplotlib.pyplot as plt
+
+
+def test_project_drr_autograd():
+    input_ = torch.rand((11, 12, 8))
+    voxel_spacing = torch.tensor([0.1, 0.2, 0.3])
+    h_matrix_inv = torch.eye(4)
+    h_matrix_inv.requires_grad = True
+    source_distance = 1000.0
+    output_size = torch.Size([10, 15])
+    detector_spacing = torch.tensor([0.2, 0.25])
+    res = autograd.project_drr(h_matrix_inv, input_, voxel_spacing, source_distance, output_size[1], output_size[0],
+                               torch.zeros(2, dtype=torch.float64), detector_spacing)
+    assert res.size() == output_size
+
+    loss_grad = torch.zeros_like(res)
+    loss_grad[5, 7] = 1.0
+    res.backward(loss_grad)
+    plt.imshow(res.detach().cpu().numpy())
+    plt.show()
+    print(h_matrix_inv.grad)
+    epsilon = 1.0e-4
+    print("epsilon =", epsilon)
+    out = torch.empty_like(h_matrix_inv.detach())
+    for j in range(4):
+        for i in range(4):
+            delta = torch.zeros((4, 4))
+            delta[j, i] = epsilon
+            h_matrix_inv2 = torch.eye(4) + delta
+            res2 = autograd.project_drr(h_matrix_inv2, input_, voxel_spacing, source_distance, output_size[1],
+                                        output_size[0], torch.zeros(2, dtype=torch.float64), detector_spacing)
+            out[j, i] = (res2[5, 7] - res[5, 7]) / epsilon
+    print(out)