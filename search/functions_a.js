635b5a2d97ead78a589ed3a3e0cb9cd3928ec48b
ewoprager <ewoprager@users.noreply.github.com>
2025-11-04 11:34:33 +0000

    deploy: a0858ad21a9704094ae5f48f142eadde9edef89a

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['normalisedcrosscorrelation_5fcpu_0',['NormalisedCrossCorrelation_CPU',['../group__pytorch__functions.html#ga7d3614bf0086f3d3602a2842a2c8d4ff',1,'reg23']]],
-  ['normalisedcrosscorrelation_5fcuda_1',['NormalisedCrossCorrelation_CUDA',['../group__pytorch__functions.html#ga33290c947ad06df3c3aea8e18914da29',1,'reg23']]],
-  ['normalisedcrosscorrelation_5fforward_5fcpu_2',['NormalisedCrossCorrelation_forward_CPU',['../group__pytorch__functions.html#gaa7099314c4a1a0ef955cc8af82a838f3',1,'reg23']]],
-  ['normalisedcrosscorrelation_5fforward_5fcuda_3',['NormalisedCrossCorrelation_forward_CUDA',['../group__pytorch__functions.html#ga8af43c692a88e63396a5c275efdf118b',1,'reg23']]]
+  ['normalisedcrosscorrelation_5fcpu_0',['NormalisedCrossCorrelation_CPU',['../group__pytorch__functions.html#ga19a628b4a351d598993ac4205e65b44e',1,'reg23']]],
+  ['normalisedcrosscorrelation_5fcuda_1',['NormalisedCrossCorrelation_CUDA',['../group__pytorch__functions.html#gaea38bec6cea02220b769b2a0e2c34f0c',1,'reg23']]]
 ];
