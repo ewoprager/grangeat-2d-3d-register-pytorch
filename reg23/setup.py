ddfd3fd89aa8f9c4e0f9e50aa81e0f1e5599268c
Edmund Prager <pragere12@gmail.com>
2025-11-07 16:54:15 +0000

    added in add tensors example

--- reg23/setup.py
+++ reg23/setup.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 from typing import Tuple
+import subprocess
 
 import pathlib
 import torch
@@ -13,6 +14,56 @@ extension_name: str = "reg23"
 cpp_version: str = "-std=c++17"
 cpu_source_files: list[str] = glob.glob("src/*.cpp") + glob.glob("src/cpu/*.cpp")
 cuda_source_files: list[str] = glob.glob("src/cuda/*.cu")
+mps_source_files: list[str] = glob.glob("src/mps/*.mm")
+
+
+def compile_metal_shaders(shader_dir: pathlib.Path, output_dir: pathlib.Path) -> pathlib.Path:
+    """
+    Requires the xcodebuild MetalToolChain to be installed.
+    :param shader_dir:
+    :param output_dir:
+    :return:
+    """
+    # compile each .metal shader into a .air file
+    output_dir.mkdir(exist_ok=True, parents=True)
+    air_files = []
+    for source in shader_dir.iterdir():
+        if source.suffix != ".metal":
+            continue
+        air = output_dir / source.with_suffix(".air").name
+        print("Compiling Metal shader: {}".format(str(source)))
+        subprocess.check_call(["xcrun", "-sdk", "macosx", "metal", "-c", str(source), "-o", str(air)])
+        air_files.append(str(air))
+
+    # compile air files into a single metallib library
+    metallib = output_dir / "default.metallib"
+    print("Linking metallib...")
+    subprocess.check_call(["xcrun", "-sdk", "macosx", "metallib", *air_files, "-o", str(metallib)])
+
+    # clean up air files
+    for air_file in air_files:
+        pathlib.Path(air_file).unlink()
+
+    # convert the library into a C-array
+    header = output_dir / "default_metallib.h"
+    header.write_text(subprocess.run(["xxd", "-i", str(metallib)], capture_output=True, text=True, check=True).stdout)
+
+    # cleaning up library
+    metallib.unlink()
+
+    return metallib
+
+
+class BuildExtensionWithMetal(BuildExtension):
+    def run(self):
+        shader_dir = pathlib.Path("src") / "mps" / "shaders"
+        output_dir = pathlib.Path("include") / "reg23_mps"
+        if shader_dir.is_dir():
+            compile_metal_shaders(shader_dir, output_dir)
+        else:
+            print("Warning: No shader directory '{}'.".format(str(shader_dir)))
+        super().run()
+
 
 debug: bool = ("--debug" in sys.argv)
 if debug:
@@ -31,9 +82,19 @@ elif no_cuda:
 else:
     print("CUDA not available, building without")
 
+use_mps: bool = hasattr(torch.backends, 'mps') and torch.backends.mps.is_available()
+if use_mps:
+    print("Building with MPS")
+else:
+    print("MPS not available, building without")
+
 extension_object = CUDAExtension if use_cuda else CppExtension
 
-source_files: list[str] = (cpu_source_files + cuda_source_files) if use_cuda else cpu_source_files
+source_files: list[str] = cpu_source_files
+if use_cuda:
+    source_files.extend(cuda_source_files)
+if use_mps:
+    source_files.extend(mps_source_files)
 
 include_dirs: list[str] = [str(this_directory / "include")]
 
@@ -55,6 +116,16 @@ extra_compile_args = {  #
     ],  #
 }
 
+if use_mps:
+    # objc compiler support
+    from distutils.unixccompiler import UnixCCompiler
+
+    if '.mm' not in UnixCCompiler.src_extensions:
+        UnixCCompiler.src_extensions.append('.mm')
+        UnixCCompiler.language_map['.mm'] = 'objc'
+
+    extra_compile_args['cxx'].extend(['-framework', 'Metal', '-framework', 'Foundation', '-ObjC++'])
+
 extra_link_args = []
 if debug:
     extra_compile_args["cxx"].extend(["-g"])
@@ -62,7 +133,7 @@ if debug:
     extra_link_args.extend(["-O0", "-g"])
     macros.append(("DEBUG", None))
 
-if torch.cuda.is_available():
+if use_cuda:
     # Only compile for the compute capability of device 0:
     os.environ["TORCH_CUDA_ARCH_LIST"] = "{}.{}".format(torch.cuda.get_device_capability(0)[0],
                                                         torch.cuda.get_device_capability(0)[1])
@@ -76,4 +147,4 @@ setup(name=extension_name,  #
                            extra_compile_args=extra_compile_args,  #
                            extra_link_args=extra_link_args)  #
       ],  #
-      cmdclass={'build_ext': BuildExtension})
+      cmdclass={'build_ext': BuildExtensionWithMetal if use_mps else BuildExtension})
---
+++ reg23/src/mps/AddTensorsMPS.mm
@@ -0,0 +1,99 @@
+#include <torch/extension.h>
+#include <Metal/Metal.h>
+#include <Foundation/Foundation.h>
+
+#include <reg23_mps/default_metallib.h>
+
+// Helper function to retrieve the `MTLBuffer` from a `torch::Tensor`.
+static inline id <MTLBuffer> getMTLBufferStorage(const torch::Tensor &tensor) {
+	return __builtin_bit_cast(id < MTLBuffer > , tensor.storage().data());
+}
+
+// Define a function to add tensors using Metal
+torch::Tensor add_tensors_metal(const torch::Tensor &a, const torch::Tensor &b) {
+
+	// Check that device is MPS
+	TORCH_INTERNAL_ASSERT(a.device().type() == torch::kMPS)
+	TORCH_INTERNAL_ASSERT(b.device().type() == torch::kMPS)
+	TORCH_CHECK(a.sizes() == b.sizes())
+
+	// Check that tensors are contiguous
+	// Contiguous means that the memory is contiguous
+	torch::Tensor aContiguous = a.contiguous();
+	torch::Tensor bContiguous = b.contiguous();
+
+	// Get the total number of elements in the tensors
+	int numElements = aContiguous.numel();
+
+	// Create an empty tensor on the MPS device to hold the result
+	torch::Tensor resultFlat = torch::empty({numElements},
+											torch::TensorOptions().dtype(torch::kFloat).device(torch::kMPS));
+
+	@autoreleasepool {
+		// Get the default Metal device
+		id <MTLDevice> device = MTLCreateSystemDefaultDevice();
+
+		NSError *error = nil;
+
+		// Load the shader binary
+		dispatch_data_t shaderBinary = dispatch_data_create(include_reg23_mps_default_metallib,
+															include_reg23_mps_default_metallib_len, NULL,
+															DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+		id <MTLLibrary> library = [device newLibraryWithData:shaderBinary error:&error];
+		if (!library) {
+			throw std::runtime_error(
+				"Error compiling Metal shader: " + std::string(error.localizedDescription.UTF8String));
+		}
+
+		id <MTLFunction> function = [library newFunctionWithName:@"addTensors"];
+		if (!function) {
+			throw std::runtime_error("Error: Metal function addTensors not found.");
+		}
+
+		// Create a Metal compute pipeline state
+		id <MTLComputePipelineState> pipelineState = [device newComputePipelineStateWithFunction:function error:nil];
+
+		// Get PyTorch's command queue
+		dispatch_queue_t serialQueue = torch::mps::get_dispatch_queue();
+
+		// Get PyTorch's Metal command buffer
+		id <MTLCommandBuffer> commandBuffer = torch::mps::get_command_buffer();
+
+		dispatch_sync(serialQueue, ^() {
+		  // Create a compute command encoder
+		  id <MTLComputeCommandEncoder> encoder = [commandBuffer computeCommandEncoder];
+
+		  // Set the compute pipeline state
+		  [encoder setComputePipelineState:pipelineState];
+
+		  // Set the buffers
+		  [encoder setBuffer:getMTLBufferStorage(aContiguous)
+					   offset:aContiguous.storage_offset()
+			  attributeStride:aContiguous.element_size()
+					  atIndex:0];
+		  [encoder setBuffer:getMTLBufferStorage(bContiguous)
+					   offset:aContiguous.storage_offset()
+			  attributeStride:aContiguous.element_size()
+					  atIndex:1];
+		  [encoder setBuffer:getMTLBufferStorage(resultFlat)
+					   offset:resultFlat.storage_offset()
+			  attributeStride:resultFlat.element_size()
+					  atIndex:2];
+
+		  // Dispatch the compute kernel
+		  MTLSize gridSize = MTLSizeMake(numElements, 1, 1);
+		  NSUInteger threadGroupSize = pipelineState.maxTotalThreadsPerThreadgroup;
+		  if (threadGroupSize > numElements) {
+			  threadGroupSize = numElements;
+		  }
+		  MTLSize threadgroupSize = MTLSizeMake(threadGroupSize, 1, 1);
+		  [encoder dispatchThreads:gridSize threadsPerThreadgroup:threadgroupSize];
+		  [encoder endEncoding];
+
+		  // Tell torch to commit the command buffer
+		  torch::mps::commit();
+		});
+	}
+
+	return resultFlat.view(a.sizes());
+}
