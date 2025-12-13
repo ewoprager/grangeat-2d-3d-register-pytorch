154c4c84104aec4160d7ce8b8f5aa4d634bb1ae4
Edmund Prager <pragere12@gmail.com>
2025-12-13 10:29:32 +0000

    a new attempt

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../"))  # make project importable
+# import os
+# import sys
+#
+# sys.path.insert(0, os.path.abspath("../"))  # make project importable
 
 project = "reg23-experiments"
 author = "Edmund Prager"
