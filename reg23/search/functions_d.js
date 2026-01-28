afa866ea7930f62ffe05143d4b0dbb3de645fa53
ewoprager <ewoprager@users.noreply.github.com>
2026-01-28 07:39:19 +0000

    deploy: ca72c7b7739f5513459c75370e615e3dd3ab453a

--- reg23/search/functions_d.js
+++ reg23/search/functions_d.js
@@ -4,11 +4,13 @@ var searchData=
   ['project_5fdrr_5fbackward_1',['project_drr_backward',['../namespacereg23_1_1autograd.html#a8fc45a5011b092e4e06b6256689c6b6c',1,'reg23.autograd.project_drr_backward()'],['../namespacereg23_1_1ops.html#a7d0dc453d47043fba33390d9f777b86a',1,'reg23.ops.project_drr_backward(torch.Tensor volume, torch.Tensor voxel_spacing, torch.Tensor homography_matrix_inverse, float source_distance, int output_width, int output_height, torch.Tensor output_offset, torch.Tensor detector_spacing, torch.Tensor d_loss_d_drr)']]],
   ['project_5fdrr_5fcuboid_5fmask_2',['project_drr_cuboid_mask',['../namespacereg23_1_1ops.html#a07bd2dc72c471ea838ad4d2c5c002889',1,'reg23::ops']]],
   ['project_5fdrr_5fsetup_5fcontext_3',['project_drr_setup_context',['../namespacereg23_1_1autograd.html#ac5a782312a3a9cf55971ba22ea45f897',1,'reg23::autograd']]],
-  ['projectdrr_5fbackward_5fcpu_4',['ProjectDRR_backward_CPU',['../group__pytorch__functions.html#gabb37d1247aefa70e9ede160d10b5fdc6',1,'reg23']]],
-  ['projectdrr_5fbackward_5fcuda_5',['ProjectDRR_backward_CUDA',['../group__pytorch__functions.html#ga8ce9434ee55d1e217c27e1bad9bc61a1',1,'reg23']]],
-  ['projectdrr_5fcpu_6',['ProjectDRR_CPU',['../group__pytorch__functions.html#gaf44a8ff8d993fef4bc2729267ac5efa9',1,'reg23']]],
-  ['projectdrr_5fcuda_7',['ProjectDRR_CUDA',['../group__pytorch__functions.html#gaacc0d8d5bd8a39dfa0547cb5a3fe4d8e',1,'reg23']]],
-  ['projectdrrcuboidmask_5fcpu_8',['ProjectDRRCuboidMask_CPU',['../group__pytorch__functions.html#ga2581397ad5b85fc99a3d572080c2aaa2',1,'reg23']]],
-  ['projectdrrcuboidmask_5fcuda_9',['ProjectDRRCuboidMask_CUDA',['../group__pytorch__functions.html#ga4a729751b03c958f0dc1629137f51236',1,'reg23']]],
-  ['pybind11_5fmodule_10',['PYBIND11_MODULE',['../namespacereg23.html#a73ace405bc1b31b592345aacdc4dd248',1,'reg23']]]
+  ['project_5fdrrs_5fbatched_4',['project_drrs_batched',['../namespacereg23_1_1ops.html#ad0dcbefa7127023d10481a740fe5d0a9',1,'reg23::ops']]],
+  ['projectdrr_5fbackward_5fcpu_5',['ProjectDRR_backward_CPU',['../group__pytorch__functions.html#gabb37d1247aefa70e9ede160d10b5fdc6',1,'reg23']]],
+  ['projectdrr_5fbackward_5fcuda_6',['ProjectDRR_backward_CUDA',['../group__pytorch__functions.html#ga8ce9434ee55d1e217c27e1bad9bc61a1',1,'reg23']]],
+  ['projectdrr_5fcpu_7',['ProjectDRR_CPU',['../group__pytorch__functions.html#gaf44a8ff8d993fef4bc2729267ac5efa9',1,'reg23']]],
+  ['projectdrr_5fcuda_8',['ProjectDRR_CUDA',['../group__pytorch__functions.html#gaacc0d8d5bd8a39dfa0547cb5a3fe4d8e',1,'reg23']]],
+  ['projectdrrcuboidmask_5fcpu_9',['ProjectDRRCuboidMask_CPU',['../group__pytorch__functions.html#ga2581397ad5b85fc99a3d572080c2aaa2',1,'reg23']]],
+  ['projectdrrcuboidmask_5fcuda_10',['ProjectDRRCuboidMask_CUDA',['../group__pytorch__functions.html#ga4a729751b03c958f0dc1629137f51236',1,'reg23']]],
+  ['projectdrrsbatched_5fcuda_11',['ProjectDRRsBatched_CUDA',['../group__pytorch__functions.html#ga2fb0c5cebeefb478d05a339f116ddd7d',1,'reg23']]],
+  ['pybind11_5fmodule_12',['PYBIND11_MODULE',['../namespacereg23.html#a73ace405bc1b31b592345aacdc4dd248',1,'reg23']]]
 ];
