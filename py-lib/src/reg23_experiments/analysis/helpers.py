7272d2c11349b3b29b586f518df7d84aba892e20
ewoprager <ep585@cam.ac.uk>
2026-01-29 16:49:34 +0000

    done

--- py-lib/src/reg23_experiments/analysis/helpers.py
+++ py-lib/src/reg23_experiments/analysis/helpers.py
@@ -4,7 +4,8 @@ import numpy as np
 import torch
 import pandas as pd
 
-__all__ = ["to_latex_scientific", "save_colourmap_for_latex", "dataframe_to_tensor"]
+__all__ = ["to_latex_scientific", "save_colourmap_for_latex", "get_axis_values_if_dataframe_rectangular_over_columns",
+           "dataframe_rectangular_columns_to_tensor"]
 
 logger = logging.getLogger(__name__)
 
@@ -30,21 +31,45 @@ def save_colourmap_for_latex(filename: str, colourmap: torch.Tensor):
     np.savetxt("data/temp/{}.dat".format(filename), rows.numpy())
 
 
-def dataframe_to_tensor(df: pd.DataFrame, *, ordered_axes: list[str], value_column: str) -> tuple[
+def get_axis_values_if_dataframe_rectangular_over_columns(  #
+        df: pd.DataFrame, *, columns: list[str]) -> (list[np.ndarray] | None):
+    # full MultiIndex for unique values
+    full_index = pd.MultiIndex.from_product([df[col].unique().tolist() for col in columns], names=columns)
+    # multiIndex from the data
+    df_index = pd.MultiIndex.from_frame(df[columns])
+    # check if the index from the data matches the full index
+    if not df_index.isin(full_index).all() or len(df_index) != len(full_index):
+        return None
+    # if it does, extract the values along each axis
+    df_index = df_index.sort_values()
+    return [df_index.levels[df_index.names.index(name)].to_numpy() for name in columns]
+
+
+def dataframe_rectangular_columns_to_tensor(df: pd.DataFrame, *, ordered_axes: list[str], value_column: str) -> tuple[
     torch.Tensor, dict[str, np.ndarray]]:
-    s = df.set_index(ordered_axes)[value_column].sort_index()
-    axis_levels = [  #
+    # set the index to be a MultiIndex derived from the existing columns named in `ordered_axes`, then take just the
+    # series for the `value_column`, and sort the rows by the index.
+    s: pd.Series = df.set_index(ordered_axes)[value_column].sort_index()
+    # use the `levels` property of the MultiIndex to get an Index object containing the unique values for each level in
+    # a list. Then extract these Index objects from the list in the order of the names given in `ordered_axes`.
+    axis_index_objects: list[pd.Index] = [  #
         s.index.levels[s.index.names.index(name)]  #
         for name in ordered_axes  #
     ]
-    full_index = pd.MultiIndex.from_product(axis_levels, names=ordered_axes)
+    # create a MultiIndex object for the full grid of values, with every combination of the values from each axis.
+    full_index = pd.MultiIndex.from_product([e.tolist() for e in axis_index_objects], names=ordered_axes)
+    # re-index the DataFrame with the full index
     s = s.reindex(full_index)
+    # check for missing values - these will be populated with nans.
     if s.isna().any():
         logger.warning("Grid is incomplete — missing coordinate combinations.")
-    axis_values = {  #
-        name: level.to_numpy()  #
-        for name, level in zip(ordered_axes, axis_levels)  #
+    # get the unique values of each axis from the index objects and store in a dict to return
+    axis_values: dict[str, np.ndarray] = {  #
+        name: index.to_numpy()  #
+        for name, index in zip(ordered_axes, axis_index_objects)  #
     }
-    axis_lengths = [len(level) for level in axis_levels]
+    # get the length of each axis from the index objects
+    axis_lengths = [len(index) for index in axis_index_objects]
+    # convert the DataFrame to a flat tensor and view with the lengths of each axis
     tensor = torch.from_numpy(s.to_numpy()).view(*axis_lengths)
     return tensor, axis_values
--- scripts/plotting/program_truncation_data_summary.py
+++ scripts/plotting/program_truncation_data_summary.py
@@ -1,44 +1,85 @@
 import argparse
+from typing import Any
+import pprint
 
-import itertools
 import pathlib
 import pandas as pd
+import numpy as np
 
 from reg23_experiments.utils import logs_setup
-from reg23_experiments.analysis.helpers import dataframe_to_tensor
+from reg23_experiments.analysis.helpers import get_axis_values_if_dataframe_rectangular_over_columns
+
+KNOWN_MEASUREMENT_COLUMNS = ["distance"]
 
 
 def main(data_dir: pathlib.Path):
-    configs = []
-    for element in data_dir.iterdir():
-        if not element.is_dir():
-            continue
+    logger.info(f"Summary of data stored in directory '{str(data_dir)}':")
+    sub_directories = [element for element in data_dir.iterdir() if element.is_dir()]
+    sub_directories = sorted(sub_directories)
+    for sub_dir in sub_directories:
         data_files = []
-        for sub_element in element.iterdir():
+        for sub_element in sub_dir.iterdir():
             if sub_element.stem.startswith("data") and sub_element.suffix == ".parquet":
                 data_files.append(sub_element)
-        config_file = element / "config.txt"
-        if config_file.is_file():
+        notes_file = sub_dir / "notes.txt"
+        if notes_file.is_file():
             if not data_files:
-                logger.warning(f"Config found in directory '{element}', but no data files.")
+                logger.warning(f"Notes found in directory '{sub_dir}', but no data files.")
                 continue
-            configs.append(config_file.read_text())
+            notes = notes_file.read_text()
         else:
             if data_files:
-                logger.warning(f"Data file(s) found in directory '{element}', but no config.")
+                logger.warning(f"Data file(s) found in directory '{sub_dir}', but no notes.")
             continue
+        string = f"\nSubdirectory '{str(sub_dir.name)}':\n\tNotes: '{notes}'"
         # read in the full DataFrame from all the files
         df = pd.concat([  #
             pd.read_parquet(data_file)  #
             for data_file in data_files  #
         ])
         # get columns that contain only one unique value
-        constant_cols = [col for col in df.columns if df[col].nunique() == 1]
-
-
+        constant_cols: list[str] = [col for col in df.columns if df[col].nunique() == 1]
+        # get the values of the constant columns as a dict
+        constants: dict[str, Any] = {  #
+            col: df[col].values[0]  #
+            for col in constant_cols  #
+        }
+        string += f"\n\tConstant variables: \n{pprint.pformat(constants, indent=8)}"
+        # get the rest of the columns
+        variable_cols: list[str] = [col for col in df.columns if col not in constant_cols]
+        # find any known measurement columns that are present, and remove them from `variable_cols`
+        measurement_cols: list[str] = []
+        for col in KNOWN_MEASUREMENT_COLUMNS:
+            if col in variable_cols:
+                variable_cols.remove(col)
+                measurement_cols.append(col)
+        if measurement_cols:
+            string += f"\n\tMeasurement variables: {measurement_cols}"
+        # check if the dataframe is rectangular over the remaining variable columns, and if so, get the axis values
+        axis_values: list[np.ndarray] | None = get_axis_values_if_dataframe_rectangular_over_columns(  #
+            df, columns=variable_cols)
+        if axis_values is not None:
+            axis_value_dict: dict[str, list] = {  #
+                name: array.tolist()  #
+                for name, array in zip(variable_cols, axis_values)  #
+            }
+            string += (f"\n\tDataFrame is rectangular over remaining variables; axis values: \n"
+                       f"{pprint.pformat(axis_value_dict, indent=8)}")
+        else:
+            string += (f"\n\tDataFrame is not rectangular over remaining variables, which are: "
+                       f"{variable_cols}")
+        string += "\n"
+        logger.info(string)
 
 
 if __name__ == "__main__":
+    # set up logger
     logger = logs_setup.setup_logger()
 
-    main()
+    # parse arguments
+    parser = argparse.ArgumentParser(description="", epilog="")
+    parser.add_argument("-d", "--data-dir", type=str, default="data/temp/program_truncation",
+                        help="Directory in which to find the data files.")
+    args = parser.parse_args()
+
+    main(data_dir=pathlib.Path(args.data_dir))
