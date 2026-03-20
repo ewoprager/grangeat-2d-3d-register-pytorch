fdbf59ae308c49f87c7b4fca66dc9e4912edf8b3
ewoprager <ewoprager@users.noreply.github.com>
2026-03-20 16:11:47 +0000

    deploy: 4cb19deeb519d98be0b329b3a75544b179b359f3

--- reg23/search/all_13.js
+++ reg23/search/all_13.js
@@ -19,5 +19,6 @@ var searchData=
   ['torch_5flibrary_16',['TORCH_LIBRARY',['../namespacereg23.html#abe8eb834b4b282865414924186f13035',1,'reg23']]],
   ['torch_5flibrary_5fimpl_17',['TORCH_LIBRARY_IMPL',['../namespacereg23.html#ad3ef09e8acc66661516d454ab4c8266c',1,'reg23']]],
   ['totensor_18',['ToTensor',['../classreg23_1_1Vec.html#ac7321309edcb14fb5b1db21962ff0510',1,'reg23::Vec']]],
-  ['types_19',['Texture-Related Types',['../group__textures.html',1,'']]]
+  ['transformations_20and_20spaces_19',['Transformations and spaces',['../conventions.html#autotoc_md8',1,'']]],
+  ['types_20',['Texture-Related Types',['../group__textures.html',1,'']]]
 ];
