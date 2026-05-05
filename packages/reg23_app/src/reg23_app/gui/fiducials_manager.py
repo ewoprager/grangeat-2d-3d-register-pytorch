added fixed image display scaling. optimisation runs but we need to get all intrinsics right for any hope of actual success

packages/reg23_app/src/reg23_app/gui/fiducials_manager.py
@@ -61,8 +61,8 @@ class FiducialsManager:
         permutation = torch.tensor([name_to_index_map[name] for name in ct_point_names])
         ct_point_vectors = ct_point_vectors[permutation.argsort()]
 
-        xray_point_vectors = np.array(xray_point_vectors)
-        ct_point_vectors = np.array(ct_point_vectors)
+        xray_point_vectors = xray_point_vectors.cpu().numpy()
+        ct_point_vectors = ct_point_vectors.cpu().numpy()
 
         source_distance: float | Error = self._dadg.get(f"{self._state.register_fiducial_xray_choice}__source_distance")
         if isinstance(source_distance, Error):
@@ -74,7 +74,7 @@ class FiducialsManager:
         def residuals(pose: np.ndarray) -> np.ndarray:
             homo_vectors = np.concat((ct_point_vectors, np.ones((ct_point_vectors.shape[0], 1))), axis=1)
             transformation = mapping_parameters_to_transformation(torch.tensor(pose))
-            transformed_points = (homo_vectors @ np.array(transformation.get_h()))[:, 0:3]
+            transformed_points = (homo_vectors @ transformation.get_h().cpu().numpy())[:, 0:3]
             from_source = transformed_points - source
             frac: torch.Tensor = np.einsum("ji,i->j", from_source, c_hat) / source_distance
             projected = np.expand_dims(frac, -1) * transformed_points[:, 0:2]
@@ -83,12 +83,11 @@ class FiducialsManager:
         current_transformation: Transformation | Error = self._dadg.get(
             f"{self._state.register_fiducial_xray_choice}__current_transformation")
         if isinstance(current_transformation, Error):
-            logger.warning(
-                f"Found no current transformation for X-ray '{self._state.register_fiducial_xray_choice}': "
-                f"{current_transformation.description}")
+            logger.warning(f"Found no current transformation for X-ray '{self._state.register_fiducial_xray_choice}': "
+                           f"{current_transformation.description}")
             initial_pose = np.zeros(6)
         else:
-            initial_pose = np.array(mapping_transformation_to_parameters(current_transformation))
+            initial_pose = mapping_transformation_to_parameters(current_transformation).cpu().numpy()
 
         result: sp.optimize.OptimizeResult = sp.optimize.least_squares(residuals, initial_pose, method="lm")
 

packages/reg23_app/src/reg23_app/gui/layers/moving_image_layer.py
@@ -38,7 +38,7 @@ class _MovingImageLayerManager:
         if event.button == 1 and self._ctx.input_manager.ctrl_pressed:  # Ctrl-left click drag
             # mouse down
             dragged = False
-            drag_start = np.array([event.position[1], -event.position[0]])
+            drag_start = np.array([event.position[-1], -event.position[-2]])
             rotation_start = scipy.spatial.transform.Rotation.from_rotvec(
                 rotvec=self._ctx.dadg.get(self._current_transformation_key).rotation.cpu().numpy())
             yield
@@ -47,7 +47,7 @@ class _MovingImageLayerManager:
                 dragged = True
 
                 delta = self._ctx.state.gui_settings.rotation_sensitivity * (
-                        np.array([event.position[1], -event.position[0]]) - drag_start)
+                        np.array([event.position[-1], -event.position[-2]]) - drag_start)
                 euler_angles = [delta[1], delta[0], 0.0]
                 rot_euler = scipy.spatial.transform.Rotation.from_euler(seq="xyz", angles=euler_angles)
                 rot_combined = rot_euler * rotation_start
@@ -74,7 +74,7 @@ class _MovingImageLayerManager:
         elif event.button == 2 and self._ctx.input_manager.ctrl_pressed:  # Ctrl-right click drag
             # mouse down
             dragged = False
-            drag_start = torch.tensor(event.position)
+            drag_start = torch.tensor(event.position[-2:])
             # rotation_start = scipy.spatial.transform.Rotation.from_rotvec(transformation.rotation.cpu().numpy())
             translation_start = self._ctx.dadg.get(self._current_transformation_key).translation[0:2].cpu()
             yield
@@ -83,7 +83,7 @@ class _MovingImageLayerManager:
                 dragged = True
 
                 delta = self._ctx.state.gui_settings.translation_sensitivity * (
-                        torch.tensor(event.position) - drag_start).flip((0,))
+                        torch.tensor(event.position[-2:]) - drag_start).flip((0,))
                 prev = self._ctx.dadg.get(self._current_transformation_key)
                 tr = prev.translation
                 tr[0:2] = (translation_start + delta).to(device=tr.device)