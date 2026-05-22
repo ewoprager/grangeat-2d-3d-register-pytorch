e77c842c7806133e396a7a82251e24ff4a0fb94c
ewoprager <ep585@cam.ac.uk>
2026-05-22 15:24:48 +0100

    started trying memory limited approach to volume loading but not sure my idea makes sense

--- packages/reg23_experiments/src/reg23_experiments/ops/volume.py
+++ packages/reg23_experiments/src/reg23_experiments/ops/volume.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 from reg23_experiments.ops.signal import gaussian_kernel_1d
@@ -45,6 +46,68 @@ def downsample_trilinear_antialiased(volume: torch.Tensor,
         mode='trilinear',  #
         recompute_scale_factor=True  #
     )[0, 0]
+
+
+def unfold_3d(volume: torch.Tensor, kernel_size: int | tuple[int, int, int],
+              stride: int | tuple[int, int, int]) -> torch.Tensor:
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size, kernel_size, kernel_size)
+    if isinstance(stride, int):
+        stride = (stride, stride, stride)
+
+    padding = [(k - s + 1) // 2 for k, s in zip(kernel_size, stride)]
+    # in x direction
+    volume = torch.cat((  #
+        volume[:, :, 0].unsqueeze(2).expand(-1, -1, padding[2]),  #
+        volume,  #
+        volume[:, :, -1].unsqueeze(2).expand(-1, -1, padding[2]),  #
+    ), dim=2)
+    # in y direction
+    volume = torch.cat((  #
+        volume[:, 0, :].unsqueeze(1).expand(-1, padding[1], -1),  #
+        volume,  #
+        volume[:, -1, :].unsqueeze(1).expand(-1, padding[1], -1),  #
+    ), dim=1)
+    # in z direction
+    volume = torch.cat((  #
+        volume[0, :, :].unsqueeze(0).expand(padding[0], -1, -1),  #
+        volume,  #
+        volume[-1, :, :].unsqueeze(0).expand(padding[0], -1, -1),  #
+    ), dim=0)
+    volume = volume.unfold(0, kernel_size[0], stride[0]).unfold(1, kernel_size[1], stride[1]).unfold(2, kernel_size[2],
+                                                                                                     stride[2])
+    return volume
+
+
+def downsample_trilinear_antialiased_memory_constrained(*, volume: torch.Tensor,
+                                                        scale_factor: float | tuple[float, float, float],
+                                                        max_bytes: int) -> torch.Tensor:
+    """
+    (n + 2a)(n + 2b)(n + 2c) = b
+    n^3 + 2(a + b + c)n^2 + 4(ab + ac + bc)n + 8abc - b = 0
+
+    :param volume:
+    :param scale_factor:
+    :param max_bytes:
+    :return:
+    """
+    if isinstance(scale_factor, float):
+        scale_factor = (scale_factor, scale_factor, scale_factor)
+
+    sigma = tuple(max((1.0 / s - 1.0) * 0.5, 0.001) for s in scale_factor)
+    cutoff_at_sigmas = 3.0  # default used in gaussian_kernel_1d
+    halo_radii = [int(cutoff_at_sigmas * s + 0.5) for s in sigma]
+    max_simultaneous_images = 2.0
+    max_bytes_per_image = float(max_bytes) / max_simultaneous_images
+    halo_radii_f = np.array(halo_radii, dtype=float)
+    a = 1.0
+    b = 2.0 * np.sum(halo_radii_f)
+    c = 4.0 * (halo_radii_f[0] * halo_radii_f[1] + halo_radii_f[1] * halo_radii_f[2] + halo_radii_f[0] * halo_radii_f[
+        2])
+    d = 8.0 * np.prod(halo_radii_f)
+    chunk_nominal_side_length = int(np.max(np.roots([a, b, c, d])))
+    kernel_size = tuple(chunk_nominal_side_length + 2 * r for r in halo_radii)
+    chunks = unfold_3d(volume, kernel_size=kernel_size, stride=chunk_nominal_side_length)
 
 
 def fit_line_3d(points: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
