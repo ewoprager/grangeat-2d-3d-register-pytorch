bdd973327fe7c577f6a1be7a06fc4b7ff004a1ec
ewoprager <ep585@cam.ac.uk>
2026-05-29 16:14:21 +0100

    started adding simpleitk

--- packages/reg23_experiments/src/reg23_experiments/ops/new_simpleitk.py
+++ packages/reg23_experiments/src/reg23_experiments/ops/new_simpleitk.py
@@ -2,11 +2,24 @@ import SimpleITK as sitk
 import kornia
 import torch
 
+__all__ = ["transform_to_parameters", "parameter_to_transform"]
 
-def transform_to_parameters(transform: sitk.Euler3DTransform) -> torch.Tensor:
-    return kornia.geometry.conversions.rotation_matrix_to_axis_angle(
-        torch.tensor(transform.GetMatrix()).reshape((3, 3)).unsqueeze(0))[0]
+_PARAMETER_SCALES = torch.tensor([32.0, 32.0, 32.0, 1.0, 1.0, 0.05])
+_PARAMETER_SCALES_INV = 1.0 / _PARAMETER_SCALES
+
+
+def transform_to_parameters(transform: sitk.Euler3DTransform, **tensor_kwargs) -> torch.Tensor:
+    rotation = kornia.geometry.conversions.rotation_matrix_to_axis_angle(
+        torch.tensor(transform.GetMatrix(), **tensor_kwargs).reshape((3, 3)).unsqueeze(0))[0]
+    translation = torch.tensor(transform.GetTranslation(), **tensor_kwargs)
+    return torch.cat((rotation, translation)) * _PARAMETER_SCALES
 
 
 def parameter_to_transform(parameters: torch.Tensor) -> sitk.Euler3DTransform:
-    pass
+    scaled = parameters.cpu() * _PARAMETER_SCALES_INV
+    rotation = scaled[0:3]
+    translation = scaled[3:6]
+    ret = sitk.Euler3DTransform()
+    ret.SetMatrix(kornia.geometry.conversions.axis_angle_to_rotation_matrix(rotation).numpy())
+    ret.SetTranslation(translation.numpy())
+    return ret
