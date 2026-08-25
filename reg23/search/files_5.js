529345e4a2623625397f3974c816f32722b99078
ewoprager <ewoprager@users.noreply.github.com>
2026-08-25 16:07:24 +0000

    deploy: 1edaf2578a154d85230f56e8a4275e30f1ff8afe

--- reg23/search/files_5.js
+++ reg23/search/files_5.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['ops_2epy_0',['ops.py',['../ops_8py.html',1,'']]]
+  ['objectivefunction_2eh_0',['ObjectiveFunction.h',['../ObjectiveFunction_8h.html',1,'']]],
+  ['objectivefunctioncuda_2ecu_1',['ObjectiveFunctionCUDA.cu',['../ObjectiveFunctionCUDA_8cu.html',1,'']]],
+  ['ops_2epy_2',['ops.py',['../ops_8py.html',1,'']]]
 ];
